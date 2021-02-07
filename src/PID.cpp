2ce7fb9dcaaf1e2b3078f67e8e46ba5040b5675d
n-a-hashjin <n.a.hashjin@gmail.com>
2021-02-07 01:00:00 +0000

    release: PID controller project
    Implementation of PID controller for maneuvering a car around a track consisting of two manually configured PID controllers for both steering and speeding of the car.

---
+++ src/PID.cpp
@@ -0,0 +1,28 @@
+#include "PID.h"
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+  i_error = 0;
+  prev_cte = 0;
+  
+}
+
+void PID::UpdateError(double cte) {
+  
+  p_error = cte;
+  i_error += cte;
+  d_error = cte - prev_cte;
+  prev_cte = cte;
+  
+}
+
+double PID::TotalError() {
+  return -(p_error * Kp + i_error * Ki + d_error * Kd);
+}
